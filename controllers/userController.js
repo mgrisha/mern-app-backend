4e20439fbb96f86296a2b20b577db109bac1f574
grisha <grisha.endorphone@gmail.com>
2022-09-16 10:14:04 +0300

    signup and bcrypt password

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,15 @@ export const loginUser = async (req, res) => {
   res.json({ msg: 'login user' });
 }
 
+// signup user
 export const signupUser = async (req, res) => {
-  res.json({ msg: 'signup user' });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  // res.json({ msg: 'signup user' });
 }
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,17 @@ const userSchema = new Schema({
 		required: true
 	}
 });
+
+// signup user method
+userSchema.statics.signup = async function (email, password) {
+	const existsEmail = await this.findOne({ email });
+	if (existsEmail) {
+		throw Error('Email already in use');
+	}
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+	const user = await this.create({ email, password: hash });
+	return user;
+}
 
 export default mongoose.model('User', userSchema);
