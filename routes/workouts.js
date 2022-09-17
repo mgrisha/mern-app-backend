591247d95a23b4ebc971814dfe76307a8192709e
grisha <grisha.endorphone@gmail.com>
2022-09-17 19:01:26 +0300

    add middleware require authorization

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,8 +1,12 @@
 import express from 'express';
 // import Workout from '../models/workoutModel.js';
 import { createWorkout, getWorkout, getWorkouts, updateWorkout, deleteWorkout } from '../controllers/workoutController.js';
+import { requireAuth } from "../middleware/requireAuth.js";
 // const express = require('express');
 const router = express.Router();
+
+//require auth for all workout routes
+router.use(requireAuth);
 
 //GET all workouts
 router.get('/', getWorkouts);
